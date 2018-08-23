f41d3a5f2f677fdecf5812fc0ee5129ee988411c
Percussive Elbow <PercussiveElbow@users.noreply.github.com>
2018-08-23 21:41:51 +0100

    Started work on searching kb for pos vulns

--- clippy.py
+++ clippy.py
@@ -38,6 +38,50 @@ def determine_os_ver(ver_string):
 	ver_string.splitlines()[1]
 	#stub
 
+def kb_search(kbs):
+	print_banner("OS Ver Possible Exploits")
+	os = "notimplementedyet"
+	vulns = [ 
+		["MS04-019",["2000 SP3","2000 SP4"],"KB842526"],
+		["MS04-011",["2000 SP2","2000 SP3","2000 SP4","XP","XP SP1"],"KB835732"],
+		["MS04-020",["2000 SP4"],"KB841872"],
+		["MS05-018",["2000 SP3","2000 SP4","XP SP1","XP SP2"],"KB890859"],
+		["MS05-055",["2000 SP4"],"KB908523"],
+		["MS06-030",["2000 SP1","2000 SP2","2000 SP3","2000 SP4","XP SP2"],"KB914389"],
+		["MS06-049",["2000 SP4"],"KB920958"],
+		["MS08-025",["2000 SP4","XP SP2","2003 SP1","2003 SP2","2008","Vista","Vista SP1"],"KB920958"],
+		["MS10-015",["2000","2000 SP1", "2000 SP2","2000 SP3","2000 SP4","XP", "XP SP1","XP SP2","XP SP3","2003", "2003 SP1","2003 SP2","2003 SP3","2008","2008 SP1","2008 SP2","Vista","Vista SP1","Vista SP2","7","7 SP1"],"KB977165"],
+		["MS10-021",["2000 SP4","XP SP2","XP SP3","2003 SP2","2008 SP2","Vista","Vista SP1","Vista SP2","7"],"KB979683"],
+		["MS10-059",["2008","2008 SP1","2008 SP2","Vista","Vista SP1","Vista SP2","7"],"KB982799"],
+		["MS10-073",["XP SP2","XP SP3","2003 SP2","2008 SP2","Vista SP1","Vista SP2","7"],"KB981957"],
+		["MS10-092",["2008","2008 SP1","2008 SP2","Vista SP1","Vista SP2","7"],"KB2305420"],
+		["MS11-011",["XP SP2","XP SP3","2003 SP2","2008 SP2","Vista SP1","Vista SP2","7"],"KB2393802"],
+		["MS11-046",["XP SP3","2003 SP2","2008 SP1","2008 SP2","Vista SP1","Vista SP2","7 SP1"],"KB2503665"],
+		["MS11-062",["XP","XP SP1","XP SP2","XP SP3","2003","2003 SP1","2003 SP2"],"KB2566454"],
+		["MS11-080",["XP SP2","XP SP3","2003 SP2"],"KB2592799"],
+		["MS13-005",["Vista SP2","2008 SP2","7","7 SP1","8","2012"],"KB2778930"],
+		["MS14-002",["XP SP3","2003 SP2"],"KB2914368"],
+		["MS14-040",["2003 SP2","2008 SP2","Vista SP2","7 SP1"],"KB2975684"],
+		["MS14-070",["2003 SP2"],"KB2989935"],
+		["MS15-051",["2003 SP2","2008 SP2","Vista SP2","7 SP1"],"KB3057191"],
+		["MS15-076",["2003 SP2","Vista SP2","2008 SP2","7","7 SP1", "8","8.1","2012"],"KB3067505"],
+		["MS16-016",["2008 SP1","2008 SP2","Vista SP2","7 SP1"],"KB3136041"],
+		["MS16-032",["2008 SP1","2008 SP2","Vista SP2","7 SP1"],"KB3143141"],
+		["MS17-017",["2008 SP2","Vista SP2","7 SP1"],"KB4013081"],
+	]
+		#["MS14-058",[],"KB3000061"],
+		#["MS15-010",["XP","2003","2008","7"],"KB3036220"],
+		#["MS15-061",["2003","2008","7","2012","8"],"KB3057839"],
+		#["MS15-075",["2003","2008","7","2012","8"],"KB3164038"],
+		## ["MS16-051",["2003","2008","7","8","2012"],"KB3057191"],
+	compat = []
+
+	for vuln in vulns:
+		if os in vuln[1] and vuln[2] not in kbs:
+			compat.append(vuln)
+	for vuln in compat:
+		print(vuln[0])
+
 greetings = [
 "escalate privileges",
 "hack the planet"
@@ -125,8 +169,7 @@ def enum():
 	list_dir(Path(os.getenv('LOCALAPPDATA')))
 
 	print_banner("WMIC: Installed Patches")
-	kbs = re.findall (r'\b[KB]\w+', system_call("wmic qfe get Caption,Description,HotFixID,InstalledOn"))
-	print (kbs)
+	kb_search(re.findall (r'\b[KB]\w+', system_call("wmic qfe get Caption,Description,HotFixID,InstalledOn")))
 
 	print_banner("WMIC: Unquoted Service Paths")
 	system_call('wmic service get name,displayname,pathname,startmode |findstr /i "Auto" |findstr /i /v "C:\Windows\\" |findstr /i /v """"')
@@ -136,10 +179,6 @@ def enum():
 		system_call('accesschk.exe -uwcqv "Authenticated Users" * /accepteula')
 	else:
 		print("accesschk.exe missing.")
-
-#def kb_search
-	#stub
-
 
 def download(url):
 	file = urllib2.urlopen(url)
