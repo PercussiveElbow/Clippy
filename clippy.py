659dabf9e93225be34ad79f56fdc07b3d77585b0
Percussive Elbow <PercussiveElbow@users.noreply.github.com>
2018-08-23 02:08:18 +0100

    Porting to python 3.x. Fixing filename. Adding progfile/appdata check

--- clippy.py
+++ clippy.py
@@ -1,8 +1,10 @@
 import subprocess
-import urllib2
+import urllib
 import random
-import time
-import os.path
+import datetime
+import re
+from pathlib import Path
+import os
 
 report = ""
 
@@ -12,15 +14,29 @@ def print_banner(title):
 	banner += "\n==============================\n"
 	global report
 	report += banner
-	print banner
+	print(banner)
 
 def system_call(command):
 	process = subprocess.Popen(command,stdout=subprocess.PIPE, stderr=subprocess.PIPE,shell=True)
 	(stdout, stderr) = process.communicate()
-	print stdout
+	stdout = stdout.decode("utf-8")
+	print(stdout)
 	global report
 	report += "\n" + stdout
 	return stdout
+
+def list_dir(path):
+	dir_string = ""
+	if path.exists():
+		for currentFile in path.iterdir():  
+			dir_string += str(currentFile) + "\n"
+	print(dir_string)
+	global report
+	report += "\n" + dir_string
+
+def determine_os_ver(ver_string):
+	ver_string.splitlines()[1]
+	#stub
 
 greetings = [
 "escalate privileges",
@@ -28,7 +44,7 @@ greetings = [
 ]
 
 greeting = "I see you're trying to " + random.choice(greetings) + ", would you like some help with that?"
-print("""
+printstr = """\
 
 < {0} >
 	     __
@@ -40,12 +56,14 @@ print("""
 	    || ||
 	    |\_/|
 	    \___/
-                    """).format(greeting) #Source https://textart.io/cowsay/clippy
+                    """.format(greeting)
+
+print(printstr) #Source https://textart.io/cowsay/clippy
 
 def enum():
 	print_banner("BASIC OS INFO")
 	system_call('ver')
-	system_call('systeminfo | findstr /B /C:"OS Name" /C:"OS Version"')
+	determine_os_ver(system_call('systeminfo | findstr /B /C:"OS Name" /C:"OS Version"'))
 	system_call('hostname')
 	system_call('net config Workstation')
 
@@ -91,27 +109,36 @@ def enum():
 
 	print_banner("FILESYSTEM: Credentials Search")
 	system_call("dir /s *pass* == *cred* == *vnc* == *.config*")
-	system_call("findstr /si password *.xml *.ini *.txt *.config")
+	# system_call("findstr /si password *.xml *.ini *.txt *.config")
 	system_call("dir /b /s unattend.xml")
 	system_call("dir /b /s web.config")
 	system_call("dir /b /s sysprep.inf")
 	system_call("dir /b /s sysprep.xml")
 	system_call("dir /b /s vnc.ini")
 
+	print_banner("PROGRAMS: Installed Programs")
+	list_dir(Path(r'C:\Program Files'))
+	list_dir(Path(r'C:\Program Files (x86)'))
+
+	print_banner("PROGRAMS: Appdata")
+	list_dir(Path(os.getenv('APPDATA')))
+	list_dir(Path(os.getenv('LOCALAPPDATA')))
+
 	print_banner("WMIC: Installed Patches")
-	system_call("wmic qfe get Caption,Description,HotFixID,InstalledOn")
+	kbs = re.findall (r'\b[KB]\w+', system_call("wmic qfe get Caption,Description,HotFixID,InstalledOn"))
+	print (kbs)
 
 	print_banner("WMIC: Unquoted Service Paths")
 	system_call('wmic service get name,displayname,pathname,startmode |findstr /i "Auto" |findstr /i /v "C:\Windows\\" |findstr /i /v """"')
 
 	print_banner("ACCESSCHK")
-	if os.path.exists('myfile.txt'):
-		system_call('accesschk.exe -uwcqv "Authenticated Users" *')
+	if Path("accesschk.exe").exists():
+		system_call('accesschk.exe -uwcqv "Authenticated Users" * /accepteula')
 	else:
-		print "Accesschk.exe missing."
-
+		print("accesschk.exe missing.")
 
 #def kb_search
+	#stub
 
 
 def download(url):
@@ -120,7 +147,7 @@ def download(url):
 	with open('filename','wb') as output:
   		output.write(file.read())
 
-def hax():
+def obvious_hax():
 	print_banner("Creating a new admin user")
 	# system_call("net user hacker hacker /add")
 	# system_call("net localgroup administrators hacker /add")
@@ -134,7 +161,7 @@ def hax():
 	# system_call('netsh firewall set opmode disable')
 
 enum()
-time = time.strftime("%x").replace('/', '.')
-text_file = open("clippy_report " + time + ".txt", "w")
+time = str(datetime.datetime.now()).replace(":","_").replace(" ","_").replace(".","_")
+text_file = open("clippy_report_" + time + ".txt", "w")
 text_file.write(report)
 text_file.close()
